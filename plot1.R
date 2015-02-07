7ea40b380595e7614cd916487606dd6cbf16cfd4
Patrick Silva <patrickalex@gmail.com>
2015-02-07 13:14:58 -0200

    Add size and unit parameters to png device to plot1.R

--- plot1.R
+++ plot1.R
@@ -2,7 +2,7 @@ require(sqldf)
 file <- c("./data/household_power_consumption.txt")
 subset <- read.csv.sql(file, header = T, sep=";", sql = "select * from file where (Date == '1/2/2007' OR Date == '2/2/2007')" )
 
-png(file="plot1.png")
+png(file="plot1.png", width = 480, height = 480, units = "px")
 
 hist(subset$Global_active_power, 
      col = "red",
