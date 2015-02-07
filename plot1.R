fb23e8b685252b03c4111f770635ac9c5b5a6c83
Patrick Silva <patrickalex@gmail.com>
2015-02-07 07:39:26 -0200

    Add hist to plot1.R

--- plot1.R
+++ plot1.R
@@ -2,4 +2,9 @@ require(sqldf)
 file <- c("./data/household_power_consumption.txt")
 subset <- read.csv.sql(file, header = T, sep=";", sql = "select * from file where (Date == '1/2/2007' OR Date == '2/2/2007')" )
 
+hist(subset$Global_active_power, 
+     col = "red",
+     main="Global Active Power",
+     xlab="Global Active Power (kilowatts)"
+)
 
