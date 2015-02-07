564481fb8d2519e9e473b6301111c7de52437a13
Patrick Silva <patrickalex@gmail.com>
2015-02-07 07:40:52 -0200

    Add device png to plot1.R

--- plot1.R
+++ plot1.R
@@ -2,9 +2,12 @@ require(sqldf)
 file <- c("./data/household_power_consumption.txt")
 subset <- read.csv.sql(file, header = T, sep=";", sql = "select * from file where (Date == '1/2/2007' OR Date == '2/2/2007')" )
 
+png(file="plot1.png")
+
 hist(subset$Global_active_power, 
      col = "red",
      main="Global Active Power",
      xlab="Global Active Power (kilowatts)"
 )
 
+dev.off()
